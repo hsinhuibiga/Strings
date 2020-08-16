5bd5eb6d5827046d18f863aea242766dbae4344a
hsinhuibiga <hsinhuibiga@gmail.com>
2020-08-16 22:01:06 +1000

    Add files via upload

---
+++ Reverse Integer.py
@@ -0,0 +1,19 @@
+#Reverse Integer
+
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0: return 0
+        flag = 1
+        if x < 0:
+            flag = -1
+            x = -x
+        r = int(str(x)[::-1])
+        if flag == 1 and r <= 2147483647:
+            return r
+        elif flag == -1 and -r >= -2147483648:
+            return -r
+        return 0
