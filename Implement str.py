a2e58ed10ecb9081567c00be1855169e407f274a
hsinhuibiga <hsinhuibiga@gmail.com>
2020-08-16 22:29:40 +1000

    Add files via upload

---
+++ Implement str.py
@@ -0,0 +1,10 @@
+#Implement strStr()
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)
